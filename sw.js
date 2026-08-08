7499218b4bc2e5edb70473519927274adbaa6ec5
LUX <ajudaasofia@gmail.com>
2026-08-08 19:00:40 -0300

    Add service worker for asset caching
    Implement service worker for caching assets
    Adaptação AP

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'lux-app-v1';
+const ARQUIVOS = [
+  './',
+  './index.html',
+  './banner.jpg',
+  './foto-ilustr.png',
+  './videobanner.mp4',
+  './vip1.png',
+  './vip2.png',
+  './vip3.png',
+  './vip4.png'
+];
+
+// Instala e guarda arquivos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ARQUIVOS))
+  );
+});
+
+// Atualiza cache
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(nomes => 
+      Promise.all(nomes.filter(n => n !== CACHE_NAME).map(c => caches.delete(c)))
+    )
+  );
+});
+
+// Busca do cache primeiro
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
